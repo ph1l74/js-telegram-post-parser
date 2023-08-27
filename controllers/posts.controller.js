431bb90a5da52e8da9aa6d92df5204f34c2a224e
Filat Astakhov <ph1l74rus@gmail.com>
2023-08-27 15:11:39 +0300

    Bugfixes

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -5,12 +5,26 @@ class PostsController {
   async getPost(req, res) {
     try {
       const { channelId, postId } = req.params;
-      const result = await api.call(
+
+      const resolvedPeer = await api.call("contacts.resolveUsername", {
+        username: channelId,
+      });
+
+      console.log(resolvedPeer);
+
+      const channel = resolvedPeer.chats.find(
+        (chat) => chat.id === resolvedPeer.peer.channel_id
+      );
+
+      console.log(channel);
+
+      const message = await api.call(
         "channels.getMessages",
         {
           channel: {
             _: "inputChannel",
-            channel_id: channelId,
+            channel_id: channel.id,
+            access_hash: channel.access_hash,
           },
           id: [
             {
@@ -21,8 +35,9 @@ class PostsController {
         },
         { syncAuth: false }
       );
-      res.json(result);
-      return result;
+      console.log(message);
+      res.json(message);
+      return message;
     } catch (error) {
       res.json(error);
       errorLogger(error);
